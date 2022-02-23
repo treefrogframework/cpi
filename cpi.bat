febee2f938dfff664ac19cba0bf24f2ea3a1a6e7
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2022-02-23 14:21:23 +0900

    new file:   cpi.bat

---
+++ cpi.bat
@@ -0,0 +1,33 @@
+@echo OFF
+
+set VCVARSBAT=""
+set VSVER=
+set VSVER2019=2019
+set VSVER2017=2017
+set VSWHERE="%PROGRAMFILES(X86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+if exist %VSWHERE% (
+  for /f "usebackq tokens=*" %%i in (`%VSWHERE% -find  **\vcvarsall.bat`) do (
+    echo %%i | find "%VSVER2019%" >NUL
+    if not ERRORLEVEL 1 (
+      set VCVARSBAT="%%i"
+      set VSVER=%VSVER2019%
+      goto :break
+    )
+    echo %%i | find "%VSVER2017%" >NUL
+    if not ERRORLEVEL 1 (
+      set VCVARSBAT="%%i"
+      set VSVER=%VSVER2017%
+      goto :break
+    )
+  )
+)
+
+:break
+if exist %VCVARSBAT% (
+  call %VCVARSBAT% amd64 >nul
+  echo Set up environment for MSVC %VSVER% usage
+)
+
+@rem Run cpi
+cpi.exe
