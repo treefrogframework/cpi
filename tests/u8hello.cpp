c80bf116fc9b51824ec96d67383cdf212a233034
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2025-07-25 16:50:59 +0900

    update

--- tests/u8hello.cpp
+++ tests/u8hello.cpp
@@ -1,12 +1,22 @@
 #include <print>
 #include <string>
 
+// delete here if supported by stdlib
+template <>
+struct std::formatter<std::u8string, char> {
+    constexpr auto parse(std::format_parse_context& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const std::u8string &str, FormatContext &ctx) const {
+        return std::format_to(ctx.out(), "{}", std::string_view(reinterpret_cast<const char*>(str.data()), str.size()));
+    }
+};
+
 int main()
 {
     std::u8string msg = u8"Hello, Hola, Olá, 你好, こんにちは";
-    std::println("{}", std::string(msg.begin(), msg.end()));
-
-    // c++26
-    //std::println("{}", msg);
+    std::println("{}", msg);
     return 0;
 }
